504ef4b479739536870befd66e2b31bd08b3ecb3
abhiram8898 <57166985+abhiram8898@users.noreply.github.com>
2024-09-25 00:35:57 +0530

    Update projects.tsx

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -61,7 +61,6 @@ const projectsData = [
     previewUrl: "",
   },
 ];
-
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
   const ref = useRef(null);
@@ -82,10 +81,9 @@ const ProjectsSection = () => {
 
   return (
     <section id="projects">
-      <h2 className="text-left md:text-center text-4xl font-bold text-white mt-8 mb-6 md:mb-12">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
